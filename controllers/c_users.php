2720024ee8a366304b5a70c979853d4646e77075
skolbasov <kolbas@MacBook-Pro-Sergej.local>
2013-10-25 01:07:12 +0400

    c_users controller change profile method

---
+++ controllers/c_users.php
@@ -0,0 +1,33 @@
+<?php
+class users_controller extends base_controller {
+
+public function __construct(){
+	parent::__construct();
+	echo "users_controller construct called<br><br>";
+}
+
+public function index(){
+echo "This is the index page";
+}
+
+public function signup(){
+	echo "This is the signup page";
+}
+
+public function login(){
+	echo "This is the login page";
+}
+
+public function logout(){
+	echo "This is the logout page";
+}
+
+public function profile($user_name = NULL){
+$this->template->content=View::instance('v_users_profile');
+$this->template->title="Profile";
+$this->template->content->user_name=$user_name;
+echo $this-template;
+
+}
+
+}
