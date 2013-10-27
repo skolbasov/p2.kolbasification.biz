4bb40b5ad9b9dec89228b59940bd91895f167a28
skolbasov <kolbas@MacBook-Pro-Sergej.local>
2013-10-27 22:55:24 +0400

    menu

--- views/_v_template.php
+++ views/_v_template.php
@@ -11,9 +11,24 @@
 </head>
 
 <body>	
+<div id='menu'>
+
+<a href='/'>Home</a>
+<?php if ($user):?>
+
+<a href='/users/logout'>Logout</a>
+<a href='/users/profile'>Profile</a>
+
+<?php else: ?>
+<a href='/users/signup'>Sign up</a>
+<a href='/users/login'>Log in</a>
+<?php endif; ?>
+
+</div>
+
+<br>
 
 	<?php if(isset($content)) echo $content; ?>
-
-	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+	
 </body>
 </html>
