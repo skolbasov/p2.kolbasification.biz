792041951948b6891fac6bbbbc8c48f1bb2e90e6
skolbasov <kolbas@MacBook-Pro-Sergej.local>
2013-10-27 22:55:34 +0400

    minor

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -7,8 +7,13 @@
 
     Password<br>
     <input type='password' name='password'>
-
     <br><br>
+<?php if(isset($error)): ?>
+<div class='error'>
+	Login failed. Please double check your email and password.
+</div>
+<br>
+<?php endif; ?>
 
     <input type='submit' value='Log in'>
 
