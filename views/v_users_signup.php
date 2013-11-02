ab0f8624bda28fbcf3d1bcba1b280d67e93fc3fe
Sergey Kolbasov <skolbasov@gmail.com>
2013-11-02 16:55:44 +0400

    input types altered

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,15 +1,15 @@
 <form method='POST' action='/users/p_signup'>
 
     First Name<br>
-    <input type='text' name='first_name'>
+    <input type='text' name='first_name' autocomplete="on">
     <br><br>
 
     Last Name<br>
-    <input type='text' name='last_name'>
+    <input type='text' name='last_name' autocomplete="on">
     <br><br>
 
     Email<br>
-    <input type='text' name='email'>
+    <input type='email' name='email' autocomplete="on">
     <br><br>
 
     Password<br>
