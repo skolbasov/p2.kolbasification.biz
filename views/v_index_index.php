9ea03934c4744c0e6e95dc997eb16b2324a3e57a
Sergey Kolbasov <skolbasov@gmail.com>
2013-11-05 00:55:12 +0400

    additional features list

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,1 +1,11 @@
 <h1>Welcome to <?=APP_NAME?><?php if($user) echo ', '.$user->first_name; ?></h1>
+
+Sblog=Simple Blog
+
+Additional features that are implemented:
+<br>
+
+Email confirmation upon sign-up
+<br>
+Email verification (requires users to click a link in their email before their account is created).
+<br>
