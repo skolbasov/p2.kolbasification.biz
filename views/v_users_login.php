12c864981fbcdb37b0a3c9561923b6aa6a14ba59
Sergey Kolbasov <skolbasov@gmail.com>
2013-11-05 00:24:48 +0400

    error message up

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,20 +1,20 @@
-<form method='POST' action='/users/p_login'>
-
-    Email<br>
-    <input type='text' name='email'>
-
-    <br><br>
-
-    Password<br>
-    <input type='password' name='password'>
-    <br><br>
 <?php if(isset($error)): ?>
 <div class='error'>
 	Login failed. Please check your email for the activation link. Please double check entered email and password. 
 </div>
 <br>
 <?php endif; ?>
+<form method='POST' action='/users/p_login'>
 
-    <input type='submit' value='Log in'>
+    Email<br>
+    <input type='text' name='email' class="InputField">
+
+    <br><br>
+
+    Password<br>
+    <input type='password' name='password' class="InputField">
+    <br><br>
+
+    <input type='submit' value='Log in' class="InputButton">
 
 </form>
