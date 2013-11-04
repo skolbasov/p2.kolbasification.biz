ad9a9a5247d21d427bbcf812f532f6893e5c3533
Sergey Kolbasov <skolbasov@gmail.com>
2013-11-05 00:24:56 +0400

    css

--- views/v_users_signup.php
+++ views/v_users_signup.php
@@ -1,28 +1,35 @@
+<?php if(isset($error)): ?>
+<div class='error'>
+    <?=$error?>
+</div>
+<br>
+<?php endif; ?>
+
 <form method='POST' action='/users/p_signup'>
 
     First Name<br>
-    <input type='text' name='first_name' autocomplete="on">
+    <input type='text' name='first_name' autocomplete="on" placeholder="First name" required class="InputField">
     <br><br>
 
     Last Name<br>
-    <input type='text' name='last_name' autocomplete="on">
+    <input type='text' name='last_name' autocomplete="on" placeholder="Last name" required class="InputField">
     <br><br>
 
     Email<br>
-    <input type='email' name='email' autocomplete="on">
+    <input type='email' name='email' autocomplete="on" placeholder="mail@domain.ext" required class="InputField">
     <br><br>
 
     Password<br>
-    <input type='password' name='password'>
+    <input type='password' name='password' required class="InputField">
     <br><br>    
     
     <input type='hidden' name='timezone'>
-    
+
+    <input type='submit' value='Sign up' class="InputButton">
+
+</form>
+
     <script>
     $('input[name='timezone']').val(jstz.determine().name());
 </script>
-
-    <input type='submit' value='Sign up'>
-
-</form>
 
