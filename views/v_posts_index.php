a90f3cd81dc6f00586eab538c0fcd181613c939a
Sergey Kolbasov <skolbasov@gmail.com>
2013-11-06 00:07:18 +0400

    minor

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -11,7 +11,7 @@
 </time>
 <p><?=$post['likes']?> liked</p>
 </article>
-    <input type='submit' value='Like it!' class="InputButton">
+    <input type='submit' value='Like it!' class="InputButton"><br>
     <input type='submit' value='Edit' class="InputButton">
 </form>
 
